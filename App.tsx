acfb6c3afdf59b5f765a9ce189c848e14c306257
Toan Vu <vutoan1245@gmail.com>
2024-10-11 18:15:28 -0400

    feat: add todo list

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Button, View, StyleSheet, SafeAreaView } from "react-native";
+
 import { Amplify } from "aws-amplify";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+
+import TodoList from "./src/TodoList";
+
 import outputs from "./amplify_outputs.json";
 
 Amplify.configure(outputs);
@@ -20,8 +24,9 @@ const App = () => {
   return (
     <Authenticator.Provider>
       <Authenticator>
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
           <SignOutButton />
+          <TodoList />
         </SafeAreaView>
       </Authenticator>
     </Authenticator.Provider>
@@ -29,6 +34,10 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+  },
   signOutButton: {
     alignSelf: "flex-end",
   },
