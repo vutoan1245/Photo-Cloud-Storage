076b2c509750eba6e8d968d61f4ab44a91409172
Toan Vu <vutoan1245@gmail.com>
2024-10-11 18:37:36 -0400

    feat: add storage permission

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,9 @@ import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
   name: "photoStorage",
+  access: (allow) => ({
+    "photos/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+  }),
 });
